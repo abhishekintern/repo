a7051e6a2ecf6878331c8bef39639661882cba54
abhishekintern <abhishekb@letsupgrade.in>
2023-05-21 00:03:01 +0530

    submodules added

--- submodules.sh
+++ submodules.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Deploy Next.js project on Vercel
-vercel
-
 # Run git submodule update with GitHub access token
 echo "Running 'git submodule update --init --recursive'..."
-export GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN
-export GIT_ASKPASS=echo
-git config --global credential.helper cache
 git submodule update --init --recursive
