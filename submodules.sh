d68be54f43c92a52d9d64e560db4cc9930e5a200
abhishekintern <abhishekb@letsupgrade.in>
2023-05-21 00:06:21 +0530

    sh file configured

--- submodules.sh
+++ submodules.sh
@@ -2,4 +2,8 @@
 
 # Run git submodule update with GitHub access token
 echo "Running 'git submodule update --init --recursive'..."
+GIT_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN
+GIT_URL="https://YOUR_GITHUB_USERNAME:${GIT_ACCESS_TOKEN}@github.com/abhishekintern/submodule1"
+export GIT_ASKPASS=echo
+git config --global credential.helper cache
 git submodule update --init --recursive
