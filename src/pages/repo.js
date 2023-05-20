b9009a44c57e8d593c0b424235df74931068bf18
abhishekintern <abhishekb@letsupgrade.in>
2023-05-20 22:57:58 +0530

    repo page added

---
+++ src/pages/repo.js
@@ -0,0 +1,15 @@
+import Module1 from "@/submodules/submodule1/src/components/Module1";
+import React from "react";
+
+const Repo = () => {
+  return (
+    <div>
+      <h1>REPO</h1>
+      <div>
+        <Module1 />
+      </div>
+    </div>
+  );
+};
+
+export default Repo;
